0835ced42ef303e371ebb4a391b2282593f0635d
Macarena Baltra <macarena.bc2011@gmail.com>
2017-05-24 10:28:52 -0400

    Validación con exito - Fono

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,22 @@
 // Validando telefono
-
-function botonValid(){
 	/*
-	var boton = document.getElementById("botonValid").value; // Conectar el id del a en html que será el boton
-	boton.addEventListener("click", validar);
+function botonValid(){
 
+	var boton = document.getElementById("botonValid").value; // Conectar el id del a en html que será el boton
+	
 	function validar(){
 	*/
 
-	var boton = document.getElementById("botonValid").value;
+	var boton = document.getElementById("botonValid");
+	boton.addEventListener("click", function(){
+
 	var input = document.getElementById("input").value;
 
 	var expresionRegular1 = /^([0-9]+){9}$/; // Telefono
-	var expresionRegular2 = /\s/; // espacios en blanco
 
-	if(boton === ""){
+	if(input === ""){
 		alert("Debe ingresar su número télefonico");
-	}else if (!boton.length.match(expresionRegular1)){
+	}else if (input != expresionRegular1){
 		alert("Debe ingresar 9 numeros");
-	}else if(boton === boton.length.match(expresionRegular2)){
-		alert("No dejes espacios en blanco");
 	}
-  
-}
-
-//match combinar o que calsa 
+}); 
